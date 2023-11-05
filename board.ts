0133e20b888d1af6b3fa79a35cef042c9b8fa8d8
Christopher Alley <calleyviola@gmail.com>
2023-11-05 09:20:09 -0500

    Draw board without canvas

--- board.ts
+++ board.ts
@@ -1,44 +1,20 @@
-const SQUARE_SIZE = 80;
-const LIGHT_SQUARE_COLOR = "#eaddd7";
-const DARK_SQUARE_COLOR = "#846358";
-const PIECE_OFFSET = 3;
+const FILES = "abcdefgh";
 
-const board = document.getElementById("chessboard") as HTMLCanvasElement;
-const ctx = board.getContext("2d") as CanvasRenderingContext2D;
+const board = document.getElementById("chessboard") as HTMLDivElement;
+const startButton = document.getElementById("start-button") as HTMLButtonElement;
 
 const drawBoard = () => {
   for (let rank = 0; rank < 8; rank++) {
     for (let file = 0; file < 8; file++) {
       const isLightSquare = (rank + file) % 2 === 0;
-
-      ctx.fillStyle = isLightSquare ? LIGHT_SQUARE_COLOR : DARK_SQUARE_COLOR;
-      ctx.fillRect(
-        rank * SQUARE_SIZE,
-        file * SQUARE_SIZE,
-        SQUARE_SIZE,
-        SQUARE_SIZE
-      );
+      const square = `<div
+        id=${FILES[file]}${8 - rank}
+        class="square ${isLightSquare ? "light" : "dark"}"></div>`;
+      board.innerHTML += square;
     }
   }
 };
 
-const drawPieces = () => {
-  const bishop = new Image();
-  bishop.addEventListener("load", () => {
-    ctx.drawImage(
-      bishop,
-      4 * SQUARE_SIZE + PIECE_OFFSET,
-      0 * SQUARE_SIZE + 2 * PIECE_OFFSET,
-      SQUARE_SIZE - 2 * PIECE_OFFSET,
-      SQUARE_SIZE - 2 * PIECE_OFFSET);
-  });
-  bishop.src = "./assets/pieces/bB.svg";
-  // const bB = new Path2D("./assets/pieces/bB.svg");
-  // console.log(bB)
-  // ctx.stroke(bB);
-};
 
 window.addEventListener("load", drawBoard);
 
-const START_BUTTON = document.getElementById("start-button") as HTMLButtonElement;
-START_BUTTON.addEventListener("click", drawPieces);
