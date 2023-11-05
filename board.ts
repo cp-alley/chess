c7a895230de2a29e948123bc019a396e5e461b27
Christopher Alley <calleyviola@gmail.com>
2023-11-04 21:11:32 -0400

    Refactor board drawing

--- board.ts
+++ board.ts
@@ -1,14 +1,20 @@
-const BOARD_WIDTH = 640;
-const BOARD_HEIGHT = 640;
+const SQUARE_SIZE = 80;
+const LIGHT_SQUARE_COLOR = "#eaddd7";
+const DARK_SQUARE_COLOR = "#846358";
 
 const board = document.getElementById("chessboard") as HTMLCanvasElement;
 const ctx = board.getContext("2d") as CanvasRenderingContext2D;
 
-for (let rank = 0; rank < 7; rank++) {
-  for (let file = 0; file < 7; file ++) {
+for (let rank = 0; rank < 8; rank++) {
+  for (let file = 0; file < 8; file++) {
     const isLightSquare = (rank + file) % 2 === 0;
 
-    ctx.fillStyle = isLightSquare ? "white" : "black";
-    ctx.fillRect(rank*80,file*80,80,80)
+    ctx.fillStyle = isLightSquare ? LIGHT_SQUARE_COLOR : DARK_SQUARE_COLOR;
+    ctx.fillRect(
+      rank * SQUARE_SIZE,
+      file * SQUARE_SIZE,
+      SQUARE_SIZE,
+      SQUARE_SIZE
+    );
   }
 }
